cc00dbeb1110845f78f1a592d35ac4bcc8b8e3b3
Connor Keating <cjkeati1@asu.edu>
2020-01-28 15:57:36 -0700

    Made route to get all your followers and to follow a user

--- routes/api/userRouter.js
+++ routes/api/userRouter.js
@@ -31,20 +31,38 @@ userRouter.get('/me/followers', auth, async (req, res) => {
 // @route PUT api/users/:id/follow
 // @desc Follow a user
 // @access Private
-postRouter.put('/:id/follow', auth, async (req, res) => {
+userRouter.put('/:id/follow', auth, async (req, res) => {
+   // Cannot follow self
+   if (req.user === req.params.id)
+      return res.status(400).json({msg: 'You cannot follow yourself'});
+
    try {
       // Find current user by ID
+      const user = await User.findById(req.user);
 
       // Find followee by ID
+      const followee = await User.findById(req.params.id);
 
       // Verify that they both exist
+      if (!user)
+         return res.status(400).json({msg: 'Token does not match any user in the database'});
+      if (!followee)
+         return res.status(400).json({msg: 'Cannot follow. A user with that ID does not exist'});
 
+      // If user has already followed the intended followee, send bad request
+      if (user.following.filter(followee => (followee.user.toString() === req.params.id)).length > 0) {
+         return res.status(400).json({msg: 'Cannot follow a user more than once'});
+      }
+      console.log('hit');
       // Add followee to current users 'following' array
-
       // Add current user to followee's 'followers' array
+      user.following.unshift({user: followee._id});
+      followee.followers.unshift({user: req.user});
 
-      // Return current user's 'following' array
+      await user.save();
+      await followee.save();
 
+      res.send(user.following);
    } catch (err) {
       if (err.kind === 'ObjectId') {
          return res.status(404).json({msg: 'Post not found'});
