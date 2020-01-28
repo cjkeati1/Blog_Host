865d6a1ebe6d13b21f8010dd1ed1a9f48977c052
Connor Keating <cjkeati1@asu.edu>
2020-01-27 20:09:14 -0700

    Added routes for current user to get their profile and also to create/update their profile

--- middleware/auth.js
+++ middleware/auth.js
@@ -16,7 +16,6 @@ module.exports = (req, res, next) => {
    try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
       req.user = decoded.user.id;
-      console.log(decoded);
       next();
    } catch (err) {
       return res.status(401).json({msg: 'Token is invalid'});
--- routes/api/profileRouter.js
+++ routes/api/profileRouter.js
@@ -5,10 +5,81 @@ const Profile = require('../../models/profile');
 const auth = require('../../middleware/auth');
 
 // @route GET api/profile/me
-// @desc Get my profile
+// @desc Get current user's profile
 // @access Private
 profileRouter.get('/me', auth, async (req, res) => {
-   res.send('Authorized');
+   try {
+      // Check if current user has a profile already
+      const profile = await Profile.findOne({user: req.user})
+         .populate('user', ['name']);
+
+      console.log(profile.user);
+
+      // If not, return bad request
+      if (!profile) {
+         return res.status(400).json({msg: 'There is no profile for your account'});
+      }
+
+      // Respond with profile
+      return res.json(profile);
+   } catch (e) {
+      console.error(e);
+      return res.status(500).send('Server Error');
+   }
+});
+
+// @route POST api/profile
+// @desc Create or update user profile
+// @access Private
+profileRouter.post('/', auth, async (req, res) => {
+   const {
+      company,
+      website,
+      location,
+      bio,
+      youtube,
+      facebook,
+      twitter,
+      instagram
+   } = req.body;
+
+   // Build profile object
+   const profileFields = {};
+   console.log(req.user.id);
+   profileFields.user = req.user;
+   if (company) profileFields.company = company;
+   if (website) profileFields.website = website;
+   if (location) profileFields.location = location;
+   if (bio) profileFields.bio = bio;
+
+   // Build social object
+   profileFields.social = {};
+   if (youtube) profileFields.social.youtube = youtube;
+   if (twitter) profileFields.social.twitter = twitter;
+   if (facebook) profileFields.social.facebook = facebook;
+   if (instagram) profileFields.social.instagram = instagram;
+
+   try {
+      // Search for the current user's profile
+      let profile = await Profile.findOne({user: req.user.id});
+
+      // If a profile with the current user's id is found, update it
+      if (profile) {
+         profile = await Profile.findOneAndUpdate(
+            {user: req.user.id},
+            {$set: profileFields},
+            {new: true}
+         );
+         return res.json(profile);
+      }
+
+      // If not, create a profile for the current user
+      profile = await Profile.create(profileFields);
+      return res.json(profile);
+   } catch (e) {
+      console.log(e);
+      return res.status(500).send('Server Error');
+   }
 });
 
 module.exports = profileRouter;
