d70c65075da904f260cf1e332a7f9ff6a15c5022
Connor Keating <cjkeati1@asu.edu>
2020-02-16 14:26:50 -0700

    Slightly changed layout on Contact page

--- client/src/components/contact/Contact.js
+++ client/src/components/contact/Contact.js
@@ -34,11 +34,11 @@ const Contact = ({sendMessage}) => {
          <div className="hero-body">
             <div className="container has-text-centered">
                <div className="columns is-8 is-variable ">
-                  <div className="column is-two-thirds has-text-left">
+                  <div className="column is-two-fifths has-text-left">
                      <h1 className="title is-1">Contact Us</h1>
                      <p className="is-size-4">Feel free to let us know about any questions/concerns you may have. </p>
                   </div>
-                  <div className="column is-one-third has-text-left">
+                  <div className="column is-three-fifths has-text-left">
                      <div className="field">
                         <label className="label">Name</label>
                         <div className="control">
